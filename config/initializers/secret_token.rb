f2d4e91d03fd8a8615f00de2b7dd17c0cbf33486
mfoxtrot <dmitrey.sokolov@gmail.com>
2015-10-25 11:39:29 +0300

    New Goods app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goods::Application.config.secret_key_base = 'f19b78be6e67ee50693217a389db3f06c2d6fb092bf85065a00e999652ac144fd6e18bde9c3338a8c9cab409494a16f8e3067780028f1034263bfd5bfd8c0175'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Goods::Application.config.session_store :cookie_store, key: '_goods_session'
