300608383178c16162196b8b2d1380c705f0efb7
mfoxtrot <dmitrey.sokolov@gmail.com>
2015-10-25 18:32:28 +0300

    part1

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
 
   belongs_to :user
   dragonfly_accessor :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image_uid, presence: true
+
+  validates_property :format, of: :image, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false,
+                   message: "should be either .jpeg, .jpg, .png, .bmp", if: :image_changed?
 end
